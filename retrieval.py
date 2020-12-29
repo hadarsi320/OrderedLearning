edce6767bb28dbefca335d4ae1e40be50827477b
Hadar <hadarsi320@gmail.com>
2020-12-29 18:28:40 +0200

    more progress on retrieval framework

--- retrieval.py
+++ retrieval.py
@@ -13,21 +13,29 @@ def main():
     mean = [0.49139968, 0.48215841, 0.44653091]
     std = [0.24703223, 0.24348513, 0.26158784]
     dataset, dataloader = data_utils.load_cifar10(100)
-    autoencoder = torch.load(open('models/nested_dropout_autoencoder_14_00_30.pkl', 'rb'))
+    autoencoder = torch.load(open('models/nested_dropout_autoencoder_14_00_30.pkl', 'rb'),
+                             map_location=torch.device('cpu'))
+    autoencoder.device = torch.device('cpu')
 
-    # data_representation = utils.get_data_representation(autoencoder, dataloader)
-    random.seed(42)
-    for sample, _ in dataset:
-        if random.random() <= 0.9:
-            continue
-        plt.imshow(utils.restore_image(sample.view(3, 32, 32), mean, std))
-        plt.show()
+    representation = utils.get_data_representation(autoencoder, dataloader)
+    print('representations created')
+    binary_representations = utils.binarize_data(representation)
+    print('binary representations created')
+    binary_tree = BinaryTree(binary_representations, tree_depth=5)
+    print('binary tree created')
 
-        with torch.no_grad():
-            reconstructed = autoencoder(sample).to('cpu')
-            plt.imshow(utils.restore_image(reconstructed.view(3, 32, 32), mean, std))
-            plt.show()
-        break
+    # random.seed(420)
+    # for sample, _ in dataset:
+    #     if random.random() <= 0.9:
+    #         continue
+    #     plt.imshow(utils.restore_image(sample.view(3, 32, 32), mean, std))
+    #     plt.show()
+    #
+    #     with torch.no_grad():
+    #         reconstructed = autoencoder(sample).to('cpu')
+    #         plt.imshow(utils.restore_image(reconstructed.view(3, 32, 32), mean, std))
+    #         plt.show()
+    #     break
 
 
 if __name__ == '__main__':
