019b918c85fee5ecac4a76928396eac964cd5cda
Hadar <hadarsi320@gmail.com>
2020-12-29 17:17:41 +0200

    Created function which binarizes a metrix

--- utils_package/utils.py
+++ utils_package/utils.py
@@ -11,3 +11,13 @@ def get_data_representation(autoencoder, dataloader):
 def restore_image(data, mean, std) -> torch.tensor:
     restored_image = torch.tensor(std).view(3, 1, 1) * data + torch.tensor(mean).view(3, 1, 1)
     return restored_image.permute(1, 2, 0)
+
+
+def binarize_data(data: torch.Tensor, q=0.5):
+    if not 0 < q < 1:
+        raise ValueError('q must be in range (0, 1)')
+
+    binarized = torch.zeros_like(data)
+    q_quantiles = torch.quantile(data, q, dim=0)
+    binarized[data >= q_quantiles] = 1
+    return binarized
