f01e4909f186e29b91735fa05030196d347436f0
Hadar <hadarsi320@gmail.com>
2020-12-29 17:32:33 +0200

    Bug fix and code clean up

--- utils_package/nn_utils.py
+++ utils_package/nn_utils.py
@@ -1,5 +1,6 @@
-import torch.nn as nn
 from math import log2, ceil, floor
+
+import torch.nn as nn
 
 
 def create_sequential(start_dim: int, end_dim: int, activation: str = None):
@@ -9,12 +10,12 @@ def create_sequential(start_dim: int, end_dim: int, activation: str = None):
     if start_dim > end_dim:
         hidden_start = floor(log2(start_dim))
         hidden_end = ceil(log2(end_dim))
-        for i in range(hidden_start, hidden_end-1, -1):
+        for i in range(hidden_start, hidden_end - 1, -1):
             dimensions.append(2 ** i)
     else:
         hidden_start = ceil(log2(start_dim))
         hidden_end = floor(log2(end_dim))
-        for i in range(hidden_start, hidden_end+1, 1):
+        for i in range(hidden_start, hidden_end + 1, 1):
             dimensions.append(2 ** i)
 
     layers = []
@@ -23,11 +24,7 @@ def create_sequential(start_dim: int, end_dim: int, activation: str = None):
         layers.append(nn.Linear(last_dim, dim))
         if activation is not None:
             layers.append(getattr(nn, activation)())
-    layers.append(nn.Linear(last_dim, dim))
+        last_dim = dim
+    layers.append(nn.Linear(last_dim, end_dim))
 
     return nn.Sequential(*layers)
-
-
-if __name__ == '__main__':
-    print(create_sequential(100, 3072, activation='ReLU'))
-    print(create_sequential(3072, 100, activation='Sigmoid'))
