217ec298b635ed7368f3c2c012d79253e0b20c5d
Hadar <hadarsi320@gmail.com>
2020-12-29 17:57:10 +0200

    binary tree now returns a mask over the data

--- data_structures/binary_tree.py
+++ data_structures/binary_tree.py
@@ -8,23 +8,28 @@ class BinaryTree:
     For node in depth i, its right son contains a subset of its items that have 1 in the i+1-th cell
     """
 
-    def __init__(self, data: torch.tensor):
+    def __init__(self, data: torch.Tensor):
         assert all(item in [0, 1] for row in data for item in row)
         self._data = data
         self._tree_depth = data.shape[1]
-        self._root = self.generate_node(self._data, depth=-1)
+        self._root = self.generate_tree()
 
-    def generate_node(self, data, depth, parent=None):
-        node = BinaryTreeNode(data, depth, parent)
+    def generate_tree(self, ):
+        initial_mask = torch.ones(self._data.shape[0], dtype=torch.bool)
+        root = self.generate_node(initial_mask, -1, None)
+        return root
+
+    def generate_node(self, mask: torch.Tensor, depth, parent):
+        node = BinaryTreeNode(mask, depth, parent)
         depth += 1
         if depth < self._tree_depth:
-            left_son_data = data[data[:, depth] == 0]
-            node.set_left_son(self.generate_node(left_son_data, depth, node))
-            right_son_data = data[data[:, depth] == 1]
-            node.set_right_son(self.generate_node(right_son_data, depth, node))
+            left_son_mask = torch.logical_and(self._data[:, depth] == 0, mask)
+            node.set_left_son(self.generate_node(left_son_mask, depth, node))
+            right_son_mask = torch.logical_and(self._data[:, depth] == 1, mask)
+            node.set_right_son(self.generate_node(right_son_mask, depth, node))
         return node
 
-    def search_tree(self, query: torch.tensor, result_size: int = None, depth: int = None):
+    def search_tree(self, query: torch.Tensor, result_size: int = None, depth: int = None):
         node = self._root
         for i, item in enumerate(query.squeeze()):
             if depth is not None and i == depth:
@@ -36,40 +41,39 @@ class BinaryTree:
                 node = node.get_left_son()
             else:
                 node = node.get_right_son()
-
         return node.get_data()
 
 
 class BinaryTreeNode:
-    def __init__(self, data: torch.tensor, index, parent):
-        self._index = index
-        self._data = data
-        self._num_samples = self._data.shape[0]
+    def __init__(self, data: torch.Tensor, depth, parent):
+        self.index = depth
+        self.data = data
+        self.num_samples = self.data.shape[0]
 
-        self._parent = parent
-        self._left_son = None
-        self._right_son = None
+        self.parent = parent
+        self.left_son = None
+        self.right_son = None
 
     def get_data(self):
-        return self._data
+        return self.data
 
     def get_parent(self):
-        return self._parent
+        return self.parent
 
     def get_left_son(self):
-        return self._left_son
+        return self.left_son
 
     def get_right_son(self):
-        return self._right_son
+        return self.right_son
 
     def get_num_samples(self):
-        return self._num_samples
+        return self.num_samples
 
     def is_leaf(self):
-        return self._left_son is None and self._right_son is None
+        return self.left_son is None and self.right_son is None
 
     def set_left_son(self, node):
-        self._left_son = node
+        self.left_son = node
 
     def set_right_son(self, node):
-        self._right_son = node
+        self.right_son = node
