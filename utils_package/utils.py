f50dbf69a84b9bc77b7e8ea8d8fe638171fe4c94
Hadar <hadarsi320@gmail.com>
2021-01-04 13:29:45 +0200

    Added function which plots the variance of an autoencoder's representation

--- utils_package/utils.py
+++ utils_package/utils.py
@@ -1,10 +1,12 @@
 import torch
+import matplotlib.pyplot as plt
 
 
 def get_data_representation(autoencoder, dataloader, device):
     with torch.no_grad():
         reps = torch.stack(
-            [autoencoder.get_representation(batch.to(device)) for batch, _ in dataloader]).view(-1, autoencoder.repr_dim)
+            [autoencoder.get_representation(batch.to(device)) for batch, _ in dataloader]).view(-1,
+                                                                                                autoencoder.repr_dim)
     return reps
 
 
@@ -21,3 +23,19 @@ def binarize_data(data: torch.Tensor, q=0.5):
     q_quantiles = torch.quantile(data, q, dim=0)
     binarized[data >= q_quantiles] = 1
     return binarized
+
+
+def plot_repr_var(autoencoder, train_loader, device,
+                  show=False, **kwargs):
+    plt.clf()
+    reprs = get_data_representation(autoencoder, train_loader, device)
+    plt.plot(torch.var(reprs, dim=0).to('cpu'))
+
+    if 'title' in kwargs:
+        plt.title(kwargs.pop('title'))
+
+    if 'savefig' in kwargs:
+        plt.title(kwargs.pop('savefig'))
+
+    if show:
+        plt.show()
