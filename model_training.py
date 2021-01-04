03514b979ff9391d7269f8fa2e7b9333438bbdd8
Hadar <hadarsi320@gmail.com>
2021-01-04 11:45:32 +0200

    Moved more of the function to fit_autoencoder

--- model_training.py
+++ model_training.py
@@ -1,5 +1,6 @@
+import os
+from datetime import datetime, timedelta
 from time import time
-from datetime import datetime
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,28 +8,18 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from utils_package import data_utils
 from nueral_networks import autoencoders
+from utils_package import data_utils
 from utils_package import utils
 
 
-def flatten(x: torch.Tensor):
-    return x.view(-1)
+def fit_autoencoder(autoencoder: autoencoders.Autoencoder, train_loader, learning_rate, epochs, model_name):
+    # TODO add code invariance
+    os.makedirs(f'checkpoints/{model_name}/')
 
-
-def plot_representation_variance(autoencoder, dataloader):
-    reps = utils.get_data_representation(autoencoder, dataloader)
-
-    reps_variance = torch.var(reps, dim=0)
-    plt.plot(reps_variance)
-    plt.show()
-
-
-def fit_autoencoder(autoencoder, train_loader, learning_rate, epochs, model_name):
     autoencoder.train(True)
-    autoencoder = autoencoder
-
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    autoencoder.to(device)
 
     optimizer = optim.Adam(autoencoder.parameters(), lr=learning_rate)
     loss_function = nn.MSELoss()
@@ -36,7 +27,7 @@ def fit_autoencoder(autoencoder, train_loader, learning_rate, epochs, model_name
     batch_print = len(train_loader) // 5
     losses = []
     for epoch in range(epochs):
-        print(f'\tEpoch {epoch + 1} ({autoencoder.conv_i}/100 converged units)')
+        print(f'\tEpoch {epoch + 1} ({autoencoder.conv_i}/{autoencoder.repr_dim} converged units)')
         batch_losses = []
         for i, (batch, _) in enumerate(train_loader):
             batch = batch.to(device)
@@ -57,53 +48,53 @@ def fit_autoencoder(autoencoder, train_loader, learning_rate, epochs, model_name
         print(f'\tTotal epoch loss {epoch_loss:.3f}\n')
 
         if (epoch + 1) % 5 == 0 or epoch == 0:
-            torch.save(autoencoder, f'checkpoints/{model_name}_epoch_{epoch+1}.pkl')
-            reps = utils.get_data_representation(autoencoder, train_loader)
-            reps_variance = torch.var(reps, dim=0)
-            plt.title(f'Representation variance- epoch {epoch + 1}')
-            plt.plot(reps_variance.cpu())
-            plt.savefig(f'plots/{model_name}_{epoch + 1}.png')
-            plt.show()
+            # reps = utils.get_data_representation(autoencoder, train_loader, device)
+            # reps_variance = torch.var(reps, dim=0)
+            # plt.title(f'Representation variance- epoch {epoch + 1}')
+            # plt.plot(reps_variance.cpu())
+            # plt.savefig(f'plots/{model_name}_{epoch + 1}.png')
+            # plt.show()
+            torch.save(autoencoder, f'checkpoints/{model_name}/epoch_{epoch + 1}.pkl')
 
         if autoencoder.converged is True:
-            print('\t\tThe autoencoder converged prematurely')
+            print('\t\tThe autoencoder has converged')
             break
-        # if epoch_loss >= last_loss:
-        #     break
-        # last_loss = epoch_loss
     print('Finished training')
-    return losses
 
-
-def main():
-    epochs = 20
-    learning_rate = 0.001
-    batch_size = 1000
-    rep_dim = 1024
-
-    model_name = 'nested_dropout_autoencoder_deep_relu_1024_' + datetime.now().strftime('%H_%M_%S')
-
-    train_dataset, train_loader = data_utils.load_cifar10(batch_size)
-    autoencoder = autoencoders.Autoencoder(3072, rep_dim, apply_nested_dropout=True, activation='ReLU', deep=True)
-    print('The number of the model\'s parameters:', sum(p.numel() for p in autoencoder.parameters()))
-    losses = fit_autoencoder(autoencoder, train_loader, learning_rate, epochs, model_name)
-    torch.save(autoencoder, f'models/{model_name}.pkl')
-
-    plt.plot(losses)
-    plt.ylabel('Losses')
-    plt.xlabel('Epochs')
-    plt.show()
-
-    # autoencoder = pickle.load(open('pickles/trained_autoencoder.pkl', 'rb'))
-
-    reps = utils.get_data_representation(autoencoder, train_loader)
+    reps = utils.get_data_representation(autoencoder, train_loader, device)
     reps_variance = torch.var(reps, dim=0)
     plt.plot(reps_variance.to('cpu'))
     plt.title('Final representation variance')
     plt.show()
+    torch.save(autoencoder, f'models/{model_name}.pkl')
+
+    return losses
+
+
+def main():
+    epochs = 100
+    learning_rate = 0.001
+    batch_size = 1000
+    rep_dim = 512
+    activation = 'ReLU'
+
+    model_name = f'nestedDropoutAutoencoder_deep_{rep_dim}_{activation}_' + datetime.now().strftime('%y_%m_%d_%H_%M_%S')
+
+    train_dataset, train_loader = data_utils.load_cifar10(batch_size)
+    autoencoder = autoencoders.Autoencoder(3072, rep_dim, apply_nested_dropout=True, activation=activation, deep=True,
+                                           nested_dropout_p=0.03)
+    print('The number of the model\'s parameters: {:,}'.format(sum(p.numel() for p in autoencoder.parameters())))
+    losses = fit_autoencoder(autoencoder, train_loader, learning_rate, epochs, model_name)
+
+    plt.plot(losses)
+    plt.ylabel('Losses')
+    plt.xlabel('Epochs')
+    plt.savefig(f'plots/{model_name}/')
+
+    # autoencoder = pickle.load(open('pickles/trained_autoencoder.pkl', 'rb'))
 
 
 if __name__ == '__main__':
     start_time = time()
     main()
-    print(f'Total run time: {time()-start_time}')
+    print(f'Total run time: {timedelta(seconds=time() - start_time)}')
