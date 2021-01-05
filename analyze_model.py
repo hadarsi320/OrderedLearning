Fixed a bug with the calculation of code variance

analyze_model.py
@@ -1,13 +1,20 @@
+import os
+
 import torch
 
+from nueral_networks.autoencoders import Autoencoder
 from utils_package import utils, data_utils
 
 
 def main():
-    autoencoder = torch.load(open('models/nestedDropoutAutoencoder_shallow_100_ReLU_21_01_04_14_19_00.pkl', 'rb'))
+    # autoencoder = torch.load(open(f'checkpoints/nestedDropoutAutoencoder_deep_512_ReLU_21_01_04_20_04_54/epoch_20.pkl',
+    #                               'rb'))
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    autoencoder = Autoencoder(3072, 512, deep=True, activation='ReLU')
+    autoencoder.eval()
+    autoencoder.to(device)
     _, train_loader = data_utils.load_cifar10()
-    utils.plot_repr_var(autoencoder, train_loader, device, show=True, title='Final Representation Variance')
+    utils.plot_repr_var(autoencoder, train_loader, device, show=True)
 
 
 if __name__ == '__main__':

train_model.py
@@ -15,7 +15,7 @@ from utils_package import data_utils, utils, nn_utils
 def check_convergence(autoencoder, batch: torch.Tensor, old_repr: torch.Tensor, unit: int, succession: list, eps: float,
                       bound: int) -> bool:
     new_repr = autoencoder.get_representation(batch)
-    if (linalg.norm(new_repr[:, unit] - old_repr[:, unit]) / batch.shape[0]) <= eps:
+    if (linalg.norm(new_repr[:, unit] - old_repr[:, unit]) / len(batch)) <= eps:
         succession[0] += 1
     else:
         succession[0] = 0
@@ -28,10 +28,12 @@ def check_convergence(autoencoder, batch: torch.Tensor, old_repr: torch.Tensor, 
 
 def fit_nested_dropout_autoencoder(autoencoder: Autoencoder, train_loader, learning_rate, epochs, model_name,
                                    nested_dropout_p=0.1, eps=1e-2, bound=20, corrupt_input=True, corrupt_p=0.1,
-                                   demand_code_invariance=True):
-    # TODO add code invariance
-    os.makedirs(f'plots/{model_name}/')
-    os.makedirs(f'checkpoints/{model_name}/')
+                                   demand_code_invariance=True, epoch_print=5, save_plots=True, save_models=True,
+                                   show_plots=False):
+    if save_plots:
+        os.makedirs(f'plots/{model_name}/')
+    if save_models:
+        os.makedirs(f'checkpoints/{model_name}/')
 
     converged_unit = 0
     nested_dropout_dist = Geometric(probs=nested_dropout_p)
@@ -50,7 +52,7 @@ def fit_nested_dropout_autoencoder(autoencoder: Autoencoder, train_loader, learn
     batch_print = len(train_loader) // 5
     losses = []
     for epoch in range(epochs):
-        print(f'\tEpoch {epoch + 1} ({converged_unit}/{autoencoder.repr_dim} converged units)')
+        print(f'\tEpoch {epoch + 1}/{epochs} ({converged_unit}/{autoencoder.repr_dim} converged units)')
         batch_losses = []
         for i, (batch, _) in enumerate(train_loader):
             optimizer.zero_grad()
@@ -65,13 +67,13 @@ def fit_nested_dropout_autoencoder(autoencoder: Autoencoder, train_loader, learn
             representation = nn_utils.nested_dropout(representation, nested_dropout_dist, converged_unit)
             reconstruction = autoencoder.get_reconstructions(representation)
             loss = loss_function(batch, reconstruction)
-            loss.backward()
 
             # code invariance
             if demand_code_invariance:
-                code_variance = nn_utils.code_variance(autoencoder, batch, representation)
-                code_variance.backward()
+                code_variance = nn_utils.estimate_code_variance(autoencoder, batch, representation)
+                loss += code_variance
 
+            loss.backward()
             optimizer.step()
 
             batch_losses.append(loss.item())
@@ -88,21 +90,30 @@ def fit_nested_dropout_autoencoder(autoencoder: Autoencoder, train_loader, learn
         losses.append(epoch_loss)
         print(f'\tTotal epoch loss {epoch_loss:.3f}\n')
 
-        if (epoch + 1) % 5 == 0 or epoch == 0:
-            # utils.plot_repr_var(autoencoder, train_loader, device, show=False,
-            #                     title=f'Representation variance- epoch {epoch + 1}',
-            #                     savefig=f'plots/{model_name}/epoch_{epoch + 1}.png',)
-            torch.save(autoencoder, f'checkpoints/{model_name}/epoch_{epoch + 1}.pkl')
+        if (epoch + 1) % epoch_print == 0 or epoch == 0:
+            kwargs = {}
+            if save_plots:
+                kwargs['savefig'] = f'plots/{model_name}/epoch_{epoch + 1}.png'
+            utils.plot_repr_var(autoencoder, train_loader, device, show=show_plots,
+                                title=f'Representation variance- epoch {epoch + 1}',
+                                **kwargs)
+            if save_models:
+                torch.save(autoencoder, f'checkpoints/{model_name}/epoch_{epoch + 1}.pkl')
 
         if converged is True:
             print('\t\tThe autoencoder has converged')
             break
     print('Finished training')
 
-    # utils.plot_repr_var(autoencoder, train_loader, device,
-    #                     savefig=f'models/{model_name}.pkl', title='Final representation variance')
-    torch.save(autoencoder, f'models/{model_name}.pkl')
+    kwargs = {}
+    if save_plots:
+        kwargs['savefig'] = f'plots/{model_name}/final.png'
+    utils.plot_repr_var(autoencoder, train_loader, device,
+                        title='Final representation variance', show=show_plots, **kwargs)
+    if save_models:
+        torch.save(autoencoder, f'models/{model_name}.pkl')
 
+    plt.clf()
     plt.plot(losses)
     plt.ylabel('Losses')
     plt.xlabel('Epochs')
@@ -110,7 +121,7 @@ def fit_nested_dropout_autoencoder(autoencoder: Autoencoder, train_loader, learn
 
 
 def main():
-    epochs = 100
+    epochs = 25
     learning_rate = 0.001
     batch_size = 1000
     rep_dim = 512
@@ -118,17 +129,18 @@ def main():
 
     train_dataset, train_loader = data_utils.load_cifar10(batch_size)
 
-    model_name = f'nestedDropoutAutoencoder_shallow_100_corrupt'
-    autoencoder = Autoencoder(3072, 100, deep=False)
-    # model_name = f'nestedDropoutAutoencoder_deep_{rep_dim}_{activation}_' + datetime.now().strftime(
-    #     '%y_%m_%d_%H_%M_%S')
-    # autoencoder = Autoencoder(3072, rep_dim, deep=True, activation=activation)
+    # model_name = f'nestedDropoutAutoencoder_shallow_{rep_dim}_corrupt_code_inv'
+    # autoencoder = Autoencoder(3072, rep_dim, deep=False)
+    # fit_nested_dropout_autoencoder(autoencoder, train_loader, learning_rate, epochs, model_name, show_plots=True,
+    #                                corrupt_input=True, demand_code_invariance=True, save_models=False, save_plots=False)
 
+    model_name = f'nestedDropoutAutoencoder_deep_{rep_dim}_{activation}_' \
+                 + datetime.now().strftime('%y_%m_%d_%H_%M_%S')
+    autoencoder = Autoencoder(3072, rep_dim, deep=True, activation=activation)
     print('The number of the model\'s parameters: {:,}'.format(sum(p.numel() for p in autoencoder.parameters())))
-    fit_nested_dropout_autoencoder(autoencoder, train_loader, learning_rate, epochs, model_name, corrupt_input=True,
-                                   demand_code_invariance=False)
-
-    # autoencoder = pickle.load(open('pickles/trained_autoencoder.pkl', 'rb'))
+    print(f'Epochs: {epochs} Batch size {batch_size} Number of batches {len(train_loader)}')
+    fit_nested_dropout_autoencoder(autoencoder, train_loader, learning_rate, epochs, model_name,
+                                   nested_dropout_p=0.03, bound=10, epoch_print=5, save_models=True, save_plots=True)
 
 
 if __name__ == '__main__':

utils_package/nn_utils.py
@@ -48,7 +48,7 @@ def nested_dropout(x: torch.Tensor, nested_dropout_dist, min_neuron: int):
     return mask * x
 
 
-def code_variance(autoencoder, batch, batch_repr=None):
+def estimate_code_variance(autoencoder, batch, batch_repr=None):
     if batch_repr is None:
         batch_repr = autoencoder.get_representation(batch)
 
@@ -56,5 +56,6 @@ def code_variance(autoencoder, batch, batch_repr=None):
     noisy_batch = batch + noise
     noisy_batch_repr = autoencoder.get_representation(noisy_batch)
 
-    code_variance = torch.sum(linalg.norm(noisy_batch_repr - batch_repr, dim=1) / linalg.norm(noise, dim=1)) / len(batch)
+    code_variance = torch.sum(
+        torch.pow(linalg.norm(noisy_batch_repr - batch_repr, dim=1) / linalg.norm(noise, dim=1), 2)) / len(batch)
     return code_variance