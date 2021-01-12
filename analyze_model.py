f6f7555499ae6a0af8ac2bf1d51caa417ed7bb18
Hadar <hadarsi320@gmail.com>
2021-01-12 11:33:38 +0200

    this module now prints for all models their parameters and loss

--- analyze_model.py
+++ analyze_model.py
@@ -1,19 +1,30 @@
 import os
 
 import torch
+import torch.nn.functional as F
 
-from nueral_networks.autoencoders import Autoencoder
-from utils_package import utils, data_utils
+from utils_package import utils, data_utils, nn_utils
 
 
 def main():
-    autoencoder: Autoencoder = \
-        torch.load(open(f'models/nestedDropoutAutoencoder_deep_deep_ReLU_21-01-07__01-18-13.pkl', 'rb'))
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    autoencoder.eval()
-    autoencoder.to(device)
-    _, train_loader = data_utils.get_cifar10_dataloader()
-    utils.plot_repr_var(autoencoder, train_loader, device, show=True)
+    device = utils.get_device()
+    _, dataloader = data_utils.get_cifar10_dataloader(1000)
+
+    for file in os.listdir('models/'):
+        if file.endswith('pt'):
+            dict = torch.load(f'models/{file}', map_location=device)
+            autoencoder = dict['autoencoder']
+            converged_unit = dict['converged_unit']
+            model_params = dict['parameters']
+            print(f'{file}:')
+            for key in model_params:
+                print(f'\t{key}: {model_params[key]}')
+            print(f'\tconverged unit: {converged_unit}')
+            loss = nn_utils.get_model_loss(autoencoder, dataloader, lambda x, y, res: F.mse_loss(x, res), device)
+            print(f'\tmodel loss: {loss:.2f}')
+            print()
+
+            utils.plot_repr_var(autoencoder, dataloader, device, title=file, show=True)
 
 
 if __name__ == '__main__':
