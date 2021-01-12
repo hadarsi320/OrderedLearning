cdef5d14b6fdd95d6b9ea830d7cff3a94b6bced6
Hadar <hadarsi320@gmail.com>
2021-01-12 15:04:32 +0200

    More work on reconstruction.py

--- reconstruction.py
+++ reconstruction.py
@@ -1,18 +1,20 @@
+import pickle
+
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from tqdm import tqdm
 
 from nueral_networks.autoencoders import Autoencoder
-from utils_package import data_utils
+from utils_package import data_utils, utils
 
 
 def main():
-    model_pickle = f'models/nestedDropoutAutoencoder_deep_ReLU_21-01-07__01-18-13.pkl'
+    model_pickle = f'models/nestedDropoutAutoencoder_shallow_ReLU_21-01-12__04-17-04_dict.pt'
 
-    dataset, dataloader = data_utils.get_cifar10_dataloader(1000)
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    autoencoder: Autoencoder = torch.load(open(model_pickle, 'rb'), map_location=device)
+    dataloader = data_utils.get_cifar10_dataloader(1000)
+    device = utils.get_device()
+    autoencoder: Autoencoder = torch.load(open(model_pickle, 'rb'), map_location=device)['autoencoder']
     autoencoder.eval()
 
     repr_dim = autoencoder.repr_dim
@@ -22,18 +24,23 @@ def main():
         _rec_loss = []
         for sample, _ in dataloader:
             sample = sample.to(device)
-            repr = autoencoder.get_representation(sample)
-            cut_repr = torch.zeros_like(repr)
-            cut_repr[:, :i+1] = repr[:, :i+1]
+            sample_rep = autoencoder.get_representation(sample)
+            cut_repr = torch.zeros_like(sample_rep)
+            cut_repr[:, :i + 1] = sample_rep[:, :i + 1]
             reconst = autoencoder.get_reconstructions(cut_repr)
-            _rec_loss.append(torch.linalg.norm(sample - reconst))
-        reconstruction_loss.append(torch.mean(torch.tensor(_rec_loss)))
+            _rec_loss.append(torch.linalg.norm(sample - reconst).item())
+        reconstruction_loss.append(np.mean(_rec_loss))
 
+    pickle.dump((reconstruction_loss, repr_dim),
+                open(f'pickles/reconstruction_loss_{utils.current_time()}.pkl', 'wb'))
+
+    plt.plot(range(1, repr_dim + 1), reconstruction_loss)
     plt.xlabel('Representation Bytes')
     plt.ylabel('Reconstruction Error')
+    plt.xticks(list(range(0, repr_dim, 100)) + [repr_dim])
     plt.title('Reconstruction Error by Representation Bytes')
-    plt.plot(range(1, repr_dim + 1), reconstruction_loss)
-    plt.savefig('Reconstruction Error')
+    plt.savefig('plots/reconstruction_error')
+    plt.show()
 
 
 if __name__ == '__main__':
