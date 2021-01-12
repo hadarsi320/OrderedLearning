77a5990b64d16818d925ef94fe2ea4676c7e40d0
Hadar <hadarsi320@gmail.com>
2021-01-13 01:32:49 +0200

    Clean up

--- utils_package/data_utils.py
+++ utils_package/data_utils.py
@@ -11,8 +11,8 @@ def get_cifar10_dataloader(batch_size=1):
     transform = transforms.Compose([transforms.ToTensor(),
                                     transforms.Normalize(CIFAR10_MEAN, CIFAR10_STD),
                                     transforms.Lambda(lambda t: t.view(-1))])
-    dataset = torchvision.datasets.CIFAR10(root='data/', download=True,
-                                           transform=transform, train=True)
+    dataset = torchvision.datasets.CIFAR10(root='/mnt/ml-srv1/home/hadarsi/ordered_learning/data/',
+                                           download=False, transform=transform, train=True)
     if batch_size == -1:
         dataloader = DataLoader(dataset, batch_size=len(dataset), shuffle=True)
     else:
@@ -22,12 +22,5 @@ def get_cifar10_dataloader(batch_size=1):
 
 def load_cifar10(dataloader=None):
     if dataloader is None:
-        transform = transforms.Compose([transforms.ToTensor(),
-                                        transforms.Normalize(CIFAR10_MEAN, CIFAR10_STD),
-                                        transforms.Lambda(lambda t: t.view(-1))])
-        dataset = torchvision.datasets.CIFAR10(root='data/', download=True,
-                                               transform=transform, train=True)
-        return torch.stack([sample for sample, _ in dataset])
-
-    else:
-        return torch.cat([sample for sample, _ in dataloader])
+        dataloader = get_cifar10_dataloader()
+    return torch.cat([sample for sample, _ in dataloader])
