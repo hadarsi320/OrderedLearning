a3b9f6618dba81d8ff2840c6e60a5b0995511d3c
Hadar <hadarsi320@gmail.com>
2021-01-12 15:04:23 +0200

    More work on retrieval.py

--- retrieval.py
+++ retrieval.py
@@ -1,7 +1,8 @@
 import pickle
+from time import time
 
+import numpy as np
 import torch
-from torchvision.transforms import ToPILImage
 
 from data_structures.binary_tree import BinaryTree
 from nueral_networks.autoencoders import Autoencoder
@@ -9,23 +10,58 @@ from utils_package import data_utils, utils
 
 import matplotlib.pyplot as plt
 
-import random
+
+def linear_scan(item, data, repr, output_size=10) -> torch.Tensor:
+    distance = {}
+    for i, item_ in enumerate(repr):
+        distance[i] = torch.sum(torch.abs(item - item_))
+    ordered_neighbors = sorted(distance, key=lambda k: distance[k], reverse=True)
+    return data[ordered_neighbors[:output_size]]
 
 
-def linear_scan(item, data, repr):
-    for i, _item in enumerate(repr):
-        if item == _item:
-            return data[i]
-    raise ValueError('item not in repr')
+def eval_retrieval_method(data_repr: torch.Tensor, retrieval_method):
+    times = []
+    for i in range(data_repr.shape[1]):
+        _times = []
+        for sample in data_repr:
+            start = time()
+            retrieval_method(sample, i+1)
+            _times.append(time() - start)
+        times.append(np.average(_times))
+    return times
 
 
 def main():
-    model_pickle = f'models/nestedDropoutAutoencoder_deep_ReLU_21-01-07__01-18-13.pkl'
-    _, dataloader = data_utils.get_cifar10_dataloader(1000)
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    autoencoder: Autoencoder = torch.load(open(model_pickle, 'rb'), map_location=device)
+    model_pickle = 'models/nestedDropoutAutoencoder_deep_ReLU_21-01-07__01-18-13.pkl'
+    binary_tree_pickle = 'pickles/binary_tree_50.pkl'
 
-    data_repr = torch.stack([autoencoder.get_representation(batch.to(device)) for batch, _ in dataloader])
+    dataloader = data_utils.get_cifar10_dataloader(1000)
+    device = utils.get_device()
+    autoencoder: Autoencoder = torch.load(model_pickle, map_location=device)
+    data = torch.cat([sample for sample, _ in dataloader])
+    data_repr = utils.get_data_representation(autoencoder, dataloader, device).cpu()
+    binarized_repr = utils.binarize_data(data_repr, bin_quantile=0.2)
+
+    binary_tree: BinaryTree = pickle.load(open(binary_tree_pickle, 'rb'))
+
+    def linear_scan_i(sample, i):
+        return linear_scan(sample[:i], data, binarized_repr[:, i])
+    linear_scan_times = eval_retrieval_method(binarized_repr, linear_scan_i)
+
+    def tree_search_i(sample, i):
+        return binary_tree.search_tree(sample, depth=i)
+    tree_search_times = eval_retrieval_method(binarized_repr, tree_search_i)
+
+    pickle.dump((linear_scan_times, tree_search_times), open(f'pickles/search_times_{utils.current_time()}.pkl', 'wb'))
+
+    plt.plot(range(1, autoencoder.repr_dim + 1), linear_scan_times, label='Linear Scan')
+    plt.plot(range(1, autoencoder.repr_dim + 1), tree_search_times, label='Tree Search')
+    plt.xlabel('Code Length')
+    plt.ylabel('Average retrieval time per query')
+    plt.title('Retrieval time per code length')
+    plt.legend()
+    plt.savefig('plots/retrieval_time')
+
 
     # random.seed(420)
     # for sample, _ in dataset:
