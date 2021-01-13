c12921b6ad4da61b436956553e2e0b11869f4038
Hadar <hadarsi320@gmail.com>
2021-01-13 20:02:50 +0200

    changed format of printing

--- analyze_model.py
+++ analyze_model.py
@@ -10,27 +10,30 @@ def main():
     device = utils.get_device()
     dataloader = data_utils.get_cifar10_dataloader(1000)
 
-    accuracies_dict = {}
+    losses_dict = {}
     for file in os.listdir('models/'):
         if file.endswith('dict.pt'):
             model_dict = torch.load(f'models/{file}', map_location=device)
             autoencoder = model_dict['autoencoder']
-            converged_unit = model_dict['converged_unit']
             model_params = model_dict['parameters']
+
             print(f'{file}:')
             for key in model_params:
                 print(f'\t{key}: {model_params[key]}')
-            print(f'\tconverged unit: {converged_unit}')
+
+            if 'converged_unit' in model_params:
+                print(f'\tconverged unit: {model_dict["converged_unit"]}')
             loss = nn_utils.get_model_loss(autoencoder, dataloader, lambda x, y, res: F.mse_loss(x, res), device)
             print(f'\tmodel loss: {loss:.2f}')
             print()
 
-            accuracies_dict[file] = loss
+            losses_dict[file] = loss
 
-    best_models = sorted(accuracies_dict, key=lambda x: accuracies_dict[x])[:5]
+    best_models = sorted(losses_dict, key=lambda x: losses_dict[x])[:5]
     for file in best_models:
-        autoencoder = torch.load(f'models/{file}', map_location=device)['autoencoder']
-        utils.plot_repr_var(autoencoder, dataloader, device, title=file, show=True)
+        print(file, losses_dict[file])
+        # autoencoder = torch.load(f'models/{file}', map_location=device)['autoencoder']
+        # utils.plot_repr_var(autoencoder, dataloader, device, title=file, show=True)
 
 
 if __name__ == '__main__':
