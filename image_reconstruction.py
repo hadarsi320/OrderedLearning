c88183b52fd23c797a4802973d3ccbd41749fc40
Hadar <hadarsi320@gmail.com>
2021-01-13 22:27:20 +0200

    More progress on image_reconstruction.py

--- image_reconstruction.py
+++ image_reconstruction.py
@@ -11,7 +11,7 @@ def main():
     model_pickle = 'models/nestedDropoutAutoencoder_shallow_ReLU_21-01-13__02-50-53_dict.pt'
     torch.random.manual_seed(42)
     num_images = 6
-    code_lengths = [16, 64, 128, 256, -1, None]
+    code_lengths = [16, 64, 128, 256, 1024, 'Original']
 
     device = utils.get_device()
     model: Autoencoder = torch.load(model_pickle, map_location=device)['autoencoder']
@@ -22,24 +22,30 @@ def main():
     def restore(im: torch.Tensor):
         return utils.restore_image(im.cpu().view(3, 32, 32), data_utils.CIFAR10_MEAN, data_utils.CIFAR10_STD)
 
+    plt.tight_layout()
     fig, axes = plt.subplots(ncols=num_images, nrows=len(code_lengths), squeeze=False, figsize=(12, 12))
-
     indices = torch.randint(len(dataset), (num_images,))
     for i, index in enumerate(indices):
         original_image, _ = dataset[index]
         encoding = model.encode(original_image.to(device))
         for code_length, axis in zip(code_lengths, axes):
-            if code_length is not None:
-                if code_length == -1:
+            if code_length == 'Original':
+                image = original_image
+            else:
+                if code_length == 'Full':
                     encoding_ = encoding
                 else:
                     encoding_ = torch.zeros_like(encoding)
                     encoding_[:code_length] = encoding[:code_length]
                 image = model.decode(encoding_)
-            else:
-                image = original_image
             axis[i].imshow(restore(image))
+            axis[i].set_xticks([])
+            axis[i].set_yticks([])
+            if i == 0:
+                axis[i].set_ylabel(code_length, fontsize=16)
+
     plt.tick_params()
+
     plt.show()
 
 
