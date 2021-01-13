1b5a8172b319faf251bff979ee50b5957fa72c45
Hadar <hadarsi320@gmail.com>
2021-01-13 09:09:23 +0200

    Now only displays the best models

--- analyze_model.py
+++ analyze_model.py
@@ -10,6 +10,7 @@ def main():
     device = utils.get_device()
     dataloader = data_utils.get_cifar10_dataloader(1000)
 
+    accuracies_dict = {}
     for file in os.listdir('models/'):
         if file.endswith('pt'):
             model_dict = torch.load(f'models/{file}', map_location=device)
@@ -24,7 +25,12 @@ def main():
             print(f'\tmodel loss: {loss:.2f}')
             print()
 
-            utils.plot_repr_var(autoencoder, dataloader, device, title=file, show=True)
+            accuracies_dict[file] = loss
+
+    best_models = sorted(accuracies_dict, key=lambda x: accuracies_dict[key], reverse=True)[:5]
+    for file in best_models:
+        autoencoder = torch.load(f'models/{file}', map_location=device)['autoencoder']
+        utils.plot_repr_var(autoencoder, dataloader, device, title=file, show=True)
 
 
 if __name__ == '__main__':
