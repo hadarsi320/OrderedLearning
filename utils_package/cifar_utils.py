6e08cf4f27b220a8c4d320415eb274b16eab788d
Hadar <hadarsi320@gmail.com>
2021-01-16 20:44:07 +0200

    Added localizations to my pc

--- utils_package/cifar_utils.py
+++ utils_package/cifar_utils.py
@@ -9,12 +9,13 @@ CIFAR10_MEAN = [0.49139968, 0.48215841, 0.44653091]
 CIFAR10_STD = [0.24703223, 0.24348513, 0.26158784]
 
 
-def get_cifar10_dataloader(batch_size=1):
+def get_cifar10_dataloader(batch_size=1, download=False):
     transform = transforms.Compose([transforms.ToTensor(),
                                     transforms.Normalize(CIFAR10_MEAN, CIFAR10_STD),
                                     transforms.Lambda(lambda t: t.view(-1))])
-    dataset = torchvision.datasets.CIFAR10(root='/mnt/ml-srv1/home/hadarsi/ordered_learning/data/',
-                                           download=False, transform=transform, train=True)
+    dataset = torchvision.datasets.CIFAR10(root='./data/', download=download, transform=transform, train=True)
+    # dataset = torchvision.datasets.CIFAR10(root='/mnt/ml-srv1/home/hadarsi/ordered_learning/data/',
+    #                                        download=False, transform=transform, train=True)
     if batch_size == -1:
         dataloader = DataLoader(dataset, batch_size=len(dataset), shuffle=True)
     else:
