5b50c5ba38d7061aa4a07b25791d4035205e6f0f
Hadar <hadarsi@campus.technion.ac.il>
2021-04-20 16:39:19 +0300

    Updated name

--- utils/constants.py
+++ utils/constants.py
@@ -1,2 +1,2 @@
 save_dir = '/mnt/ml-srv1/home/hadarsi/ordered_learning/saves/'
-CIFAR10_file = '/mnt/ml-srv1/home/hadarsi/ordered_learning/data/'
+datasets_dir = '/mnt/ml-srv1/home/hadarsi/ordered_learning/data/'
