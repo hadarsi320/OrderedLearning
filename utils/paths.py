dce960dbfc6f18b92a64a623df17661c55fcbded
Hadar <hadarsi@campus.technion.ac.il>
2021-04-26 14:39:07 +0300

    Renamed constants.py to paths.py

--- utils/paths.py
+++ utils/paths.py
@@ -1,2 +1,3 @@
 save_dir = '/mnt/ml-srv1/home/hadarsi/ordered_learning/saves/'
 datasets_dir = '/mnt/ml-srv1/home/hadarsi/ordered_learning/data/'
+imagenette_dir = '/mnt/ml-srv1/home/hadarsi/ordered_learning/datasets/imagenette2-320/'
