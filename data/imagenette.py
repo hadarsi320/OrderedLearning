1523a963073fd03276878308a20e58f42aa53b38
Hadar <hadarsi@campus.technion.ac.il>
2021-05-05 13:18:30 +0300

    fixed function unnormalize

--- data/imagenette.py
+++ data/imagenette.py
@@ -7,7 +7,7 @@ import utils
 from data.constants import IMAGENETTE_Y_MEAN, IMAGENETTE_Y_STD
 
 
-def get_dataloader(batch_size=1, normalize=True, image_mode='RGB'):
+def get_dataloader(batch_size=1, normalize=True, image_mode='Y'):
     transform_list = []
     if image_mode in ['Y', 'YCbCr']:
         transform_list.append(transforms.Lambda(lambda image: image.convert('YCbCr')))
@@ -34,5 +34,10 @@ def load_data(dataloader=None, normalize=True):
     return torch.cat([sample for sample, _ in dataloader])
 
 
-def unnormalize(im: torch.Tensor):
-    return utils.restore_image(im.cpu().view(3, 32, 32), IMAGENETTE_Y_MEAN, IMAGENETTE_Y_STD)
+def unnormalize(im: torch.Tensor, im_format):
+    if im.dim() == 2:
+        im = im.unsqueeze(0)
+    if im.shape == (1, 224, 224):
+        return utils.restore_image(im, IMAGENETTE_Y_MEAN, IMAGENETTE_Y_STD, im_format)
+    else:
+        raise NotImplementedError('Unnormalization not implemented for said shape')
