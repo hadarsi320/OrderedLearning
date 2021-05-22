f18f7e4105274694975ad698e956050bb29fe30f
Hadar <hadarsi@campus.technion.ac.il>
2021-05-22 12:47:42 +0300

    Created a function which finds a squarish version of some shape

--- utils/math_utils.py
+++ utils/math_utils.py
@@ -12,3 +12,12 @@ def get_power_successor(n, base=2):
     if math.log(n, base).is_integer():
         return n * base
     return pow(base, math.ceil(math.log(n, base)))
+
+
+def square_shape(x, y):
+    """Approximated square proportions"""
+    total = x * y
+    div = math.ceil(total ** 0.5)
+    while total % div != 0:
+        div += 1
+    return div, total // div
