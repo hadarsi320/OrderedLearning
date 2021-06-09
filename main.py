8b189b50c5e5e4d345fca985e52c1eebf357362c
Hadar <hadarsi@campus.technion.ac.il>
2021-06-09 09:26:07 +0300

    Running experiments on lr

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from datetime import timedelta
 from time import time
 
+from clearml import Task
+
 import utils
 from training import cnn_autoencoder
 
 # TODO create a nested dropout module
 if __name__ == '__main__':
     start_time = time()
-    cfg = utils.load_yaml('configs/convolutional_autoencoder_F.yaml')
-    cnn_autoencoder.train_cae(cfg)
-    # classifier.train_classifier()
+    original_cfg = utils.load_yaml('configs/convolutional_autoencoder_F.yaml')
+    for lr in [5e-2, 1e-2, 5e-3, 1e-3]:
+        cfg = original_cfg.copy()
+        cfg['optim']['optimizer_kwargs']['lr'] = lr
+
+        task = Task.init(project_name="Ordered Learning")
+        task.connect_configuration(cfg, name='Model Configuration')
+        cnn_autoencoder.train_cae(cfg, task)
+        task.close()
     print(f'Total run time: {timedelta(seconds=time() - start_time)}')
