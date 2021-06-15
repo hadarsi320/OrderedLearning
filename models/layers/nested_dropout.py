f8fd52f52f20d2a930ef9e63afa6943ff5eddb7a
Hadar <hadarsi@campus.technion.ac.il>
2021-06-15 20:06:46 +0300

    Added layer freezing, and now convergence is only using the last layer

--- models/layers/nested_dropout.py
+++ models/layers/nested_dropout.py
@@ -7,7 +7,7 @@ import utils
 __all__ = ['NestedDropout']
 
 
-# TODO create a __str__ function for this object
+# TODO create an __str__ function for this module
 class NestedDropout(nn.Module):
     def __init__(self, p=0.1, optimize_dropout=True, tol=1e-3, sequence_bound=2 ** 4, **kwargs):
         super(NestedDropout, self).__init__()
@@ -19,12 +19,20 @@ class NestedDropout(nn.Module):
 
         if self.optimize_dropout:
             self._tol = tol
+            self._freeze_gradients = freeze_gradients
             self._sequence = 0
             self._sequence_bound = sequence_bound
             self._old_repr = None
 
     def forward(self, x):
         if self.training and not self._has_converged:
+            if self.optimize_dropout and self._old_repr is not None:
+                self.check_convergence(x)
+                self._old_repr = None
+                return x.detach()
+                # detaching so no gradients flow when checking convergence
+
+            self._old_repr = x
             batch_size = x.shape[0]
             if self._dropout_dim is None:
                 self._dropout_dim = x.shape[1]
@@ -36,16 +44,13 @@ class NestedDropout(nn.Module):
             mask = utils.fit_dim(mask, x).to(x.device)
             x = mask * x
 
-            if self.optimize_dropout:
-                if self._old_repr is None:
-                    self._old_repr = x
-                else:
-                    self.check_convergence(x)
-                    self._old_repr = None
+            if self.optimize_dropout and self._freeze_gradients:
+                x[:, :self._converged_unit] = x[:, :self._converged_unit].detach()
         return x
 
     def check_convergence(self, x):
-        z = (x - self._old_repr)[:, :self._converged_unit + 1].reshape(-1)
+        # z = (x - self._old_repr)[:, :self._converged_unit + 1].reshape(-1)
+        z = (x - self._old_repr)[:, self._converged_unit].reshape(-1)
         difference = linalg.norm(z) / len(z)
 
         if difference <= self._tol:
