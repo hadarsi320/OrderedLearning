05d9a09385b1c6d7a36bcf9ebf95539d2a802bee
Hadar <hadarsi@campus.technion.ac.il>
2021-06-21 16:00:33 +0300

    training

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ if __name__ == '__main__':
     start_time = time()
     original_cfg = utils.load_yaml('configs/conv_ae_vanilla.yaml')
 
-    for mode in ['F', 'G', 'H']:
+    for optimizer in ['Ranger', 'Adam']:
         cfg = original_cfg.copy()
-        cfg['model']['mode'] = mode
+        cfg['optim']['optimizer'] = optimizer
 
         task = Task.init(project_name='Ordered Learning')
         task.connect_configuration(cfg, name='Model Configuration')
