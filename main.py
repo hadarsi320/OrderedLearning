e17721a2be381ac327f73bd648c57472ab366e49
Hadar <hadarsi320@gmail.com>
2021-06-22 16:58:09 +0300

    Runs

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ if __name__ == '__main__':
     original_cfg = utils.load_yaml('configs/conv_ae_vanilla.yaml')
 
     for optimizer in ['Ranger', 'Adam']:
-        cfg = original_cfg.copy()
-        cfg['optim']['optimizer'] = optimizer
+        for lr in [1e-2, 5e-3, 1e-3]:
+            cfg = original_cfg.copy()
+            cfg['optim']['optimizer'] = optimizer
+            cfg['optim']['optimizer_kwargs']['lr'] = lr
 
-        task = Task.init(project_name='Ordered Learning')
-        task.connect_configuration(cfg, name='Model Configuration')
-        cnn_autoencoder.train_cae(cfg, task)
-        task.close()
-        print(f'Total run time: {timedelta(seconds=time() - start_time)}')
+            task = Task.init(project_name='Ordered Learning')
+            task.connect_configuration(cfg, name='Model Configuration')
+            cnn_autoencoder.train_cae(cfg, task)
+            task.close()
+
+    print(f'Total run time: {timedelta(seconds=time() - start_time)}')
