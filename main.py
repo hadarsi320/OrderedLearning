cdf3744528307a6904c2514357b67ed8330229a6
Hadar <hadarsi@campus.technion.ac.il>
2021-06-30 19:21:23 +0300

    Train files

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
     start_time = time()
     original_cfg = utils.load_yaml('configs/conv_ae_vanilla.yaml')
 
-    for optimizer in ['Ranger', 'Adam']:
-        for lr in [1e-2, 5e-3, 1e-3]:
+    for filter_prod_mode in ['frobenius', 'hadamund']:
+        for lam in [1e-1, 1e-2]:
             cfg = original_cfg.copy()
-            cfg['optim']['optimizer'] = optimizer
-            cfg['optim']['optimizer_kwargs']['lr'] = lr
+            cfg['train']['filter_prod_mode'] = filter_prod_mode
+            cfg['train']['lam'] = lam
 
             task = Task.init(project_name='Ordered Learning')
             task.connect_configuration(cfg, name='Model Configuration')
