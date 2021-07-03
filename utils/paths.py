9eaf67b769f90fdd91ca0750847bf18c25c73566
Hadar <hadarsi@campus.technion.ac.il>
2021-07-03 17:21:09 +0300

    Changed 'evaluation' to 'validation'

--- utils/paths.py
+++ utils/paths.py
@@ -4,10 +4,10 @@ if socket.gethostname() == 'Hadars_Laptop':
     save_dir = r'C:\Users\Hadar\PycharmProjects\OrderedLearning\saves'
     datasets_dir = r'C:\Users\Hadar\PycharmProjects\OrderedLearning\datasets'
     imagenette_train_dir = r'C:\Users\Hadar\PycharmProjects\OrderedLearning\datasets\imagenette2-320/train/'
-    imagenette_eval_dir = r'C:\Users\Hadar\PycharmProjects\OrderedLearning\datasets\imagenette2-320/val/'
+    imagenette_val_dir = r'C:\Users\Hadar\PycharmProjects\OrderedLearning\datasets\imagenette2-320/val/'
 
 else:
     save_dir = '/mnt/ml-srv1/home/hadarsi/ordered_learning/saves/'
     datasets_dir = '/mnt/ml-srv1/home/hadarsi/ordered_learning/datasets/'
     imagenette_train_dir = '/mnt/ml-srv1/home/hadarsi/ordered_learning/datasets/imagenette2-320/train/'
-    imagenette_eval_dir = '/mnt/ml-srv1/home/hadarsi/ordered_learning/datasets/imagenette2-320/val/'
+    imagenette_val_dir = '/mnt/ml-srv1/home/hadarsi/ordered_learning/datasets/imagenette2-320/val/'
